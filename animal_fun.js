7a9076fc46a7bc2f1a285c60593e9a6e793642c1
David Harris <dahveed15@gmail.com>
2018-04-26 10:56:02 -0700

    first time using writeFile method

--- animal_fun.js
+++ animal_fun.js
@@ -11,13 +11,13 @@ fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   console.log(data);
 });
 
-// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
-//   if (err) {
-//     console.log(err)
-//     return
-//   }
-//   console.log('file successfully written')
-// })
+fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('file successfully written');
+});
 
 // function selectAnimals(animalString, animalLetter) {
 //   return animalString
